37eadad83c12271fc7b0e26a8d6f790826b09444
Pilmato <eliosavoine2014@gmail.com>
2025-06-03 11:50:43 -0300

    Update dos arquivos de listas com coisa nova
    wowowow

--- Python/lista.py
+++ Python/lista.py
@@ -33,14 +33,31 @@ print(num)
 '''
 
 #Inserir valores em uma lista vazia pelo usuário
-
+'''
 valores =[]
 for cont in range(0,5):
     valores.append(int(input("Digite um valor:")))
 
 for indice, valor in enumerate(valores):                                                                            
     print(f"Na poição {indice} foi digitado o valor {valor}")      
-    
+'''    
 #No enumerate começa a contar primeiro o indice e depois o valor, sendo a posição importante
 #por causa que na primeira posição é o indice (onde a variável indice ta escrita) e na segunda é o valor (que ta escrito a variável valor)
-    
+
+#LISTA DE LISTAS
+'''
+pessoas = [["Pedro", 25],["Maria", 19],["João",32]]            #Cada elemento dentro do "[]" tem uma posição, o Pedro tem como posição 0 e o numero tem posição 2, em cada matriz ou lista tanto faz sempre começa do 0.
+print(pessoas)                                                 # nessa lista então começando pelo Pedro ele é a matriz 0 com dois elementos iguais as outras matrizes, a Maria é a matriz 1 e o João é a matriz 3
+print(pessoas [0] [0])
+print(pessoas [2] [0])
+print(pessoas [1])
+'''
+
+pessoas = []
+cliente = []
+for indice in range (0,3):
+    cliente.append(str(input("Nome:")))
+    cliente.append(int(input("Idade:")))
+    pessoas.append(cliente[:])
+    cliente.clear()
+print(pessoas)
--- Python/listaexercício.py
+++ Python/listaexercício.py
@@ -2,16 +2,15 @@
 lista = []
 while True:
     x = int(input("Digite um valor:"))
-    if x in lista:
-        print("Valor duplicado! Não vou adicionar....")
+    if x not in lista:
+        print("Valor adicionado com sucesso...")
+        lista.append (x)
     else:
-         print("Valor adicionado com sucesso...")
-         lista.append (x)
-    lista.sort()
-    opcao = str(input("Deseja continuar? [S/N]:")) .capitalize()
+        print("Valor duplicado! Não vou adicionar....") 
+    opcao = str(input("Deseja continuar? [S/N]:")) .upper()
     if opcao == "N":
         break
-print (lista)    
+print (sorted(lista))    
 '''
 '''
 lista = []
@@ -34,7 +33,7 @@ print(lista)
 print(lista_pares)
 print(lista_impares)
 '''
-
+'''
 lista = [-1,6,-9,-8,4,0,-3,2,7,1,8,-2]
 listaordernada = [-1,6,-9,-8,4,0,-3,2,7,1,8,-2]
 positivos = [x for x in lista if x >= 0]
@@ -44,3 +43,33 @@ print(f"Lista original: {lista}")
 print(f"Lista ordenada: {listaordernada}")
 print(f"Lista positivos: {positivos}")
 print(f"Lista negativos: {negativos}")
+'''
+'''
+texto = str(input("Digite um texto:"))
+maiuscula = []
+minuscula = []
+print(texto2)
+for letra in texto2:
+  if letra.isupper():
+    maiuscula.append(letra)
+  elif letra .islower():
+    minuscula.append(letra)
+
+print(f'As letras maiusculas foram {maiuscula}\nA quantidade de letras maiusculas foram {len(maiuscula)}')
+
+print(f'As letras minusculas foram {minuscula}\nA quantidade de letras minusculas foram {len(minuscula)}')
+'''
+
+# Ex de lista de listas começa apartir daq
+
+matriz = []
+for i in range (2):
+    linha= []
+    for j in range(3):
+        valor = int(input(f"Digite um valor para a posição {[(i)]} {[(j)]}:"))
+        linha.append(valor)
+    matriz.append(linha)
+
+print("\nMatriz")
+for linha in matriz:
+    print(linha,"")
