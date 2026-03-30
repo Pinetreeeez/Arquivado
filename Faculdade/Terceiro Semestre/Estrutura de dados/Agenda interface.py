68e87bccef2f98718762858812b87d377509c916
Pilmalooo <eliosavoine2014@gmail.com>
2026-03-30 19:35:56 -0300

    progresso

--- Faculdade/Terceiro Semestre/Estrutura de dados/Agenda interface.py
+++ Faculdade/Terceiro Semestre/Estrutura de dados/Agenda interface.py
@@ -3,28 +3,47 @@ def inserir(CodigoN, NomeN, TelefoneN):
     "Codigo": f"{CodigoN}",
     "Nome": f"{NomeN}",
     "Telefone": f"{TelefoneN}",
-}]
-    for x,y in contato:
-        print(f"{x}:{y}")
+    }]
     return contato
+    
+def mostar():
+    print ("Aqui estão todos os contatos!")
+    return inserir
 
-   
-print('''Ola! Bem vindo á agenda de contatos! Todas as funções estão a baixo. \n Digite [1] para Inserir um contato.
-        \n Digite [2] para Mostrar todos os contatos.
-        \n Digite [3] para Buscar um contato.
-        \n Digite [4] para Editar o contato.
-        \n Digite [5] para Ordenar os contatos por nome.
-        \n Digite [6] para Apagar o contato.
-        \n Digite [7] para Sair da agenda.
-        ''') 
-    
-escolha = int(input("Digite a opção que deseja acessar: "))
-if escolha == 1:
-    print("ola")
-elif escolha == 2:
-    print ("Ola")
-elif escolha == 3:
-    print("Ola")
-elif escolha == 4:
-    print("Ola")
-    
+deu_print = False
+while True:  
+    if not deu_print:
+        print('''Ola! Bem vindo á agenda de contatos! Todas as funções estão a baixo.
+            \n Digite [1] para Inserir um contato.
+            \n Digite [2] para Mostrar todos os contatos.
+            \n Digite [3] para Buscar um contato.
+            \n Digite [4] para Editar o contato.
+            \n Digite [5] para Ordenar os contatos por nome.
+            \n Digite [6] para Apagar o contato.
+            \n Digite [7] para Sair da agenda.
+            ''') 
+        deu_print = True        #Condição pra dar print só uma vez
+
+    escolha = int(input("Digite um dos números para realizar a função que busca: "))
+
+    if escolha == 1:
+        codigo = input("Digite o seu código: ")
+        nome = input("Digite o nome: ")
+        telefone = input("Digite o telefone: ")
+        print(inserir(codigo,nome,telefone))
+    elif escolha == 2:
+        mostrar_resultado = mostar()        #ERRADDO
+        print(mostrar_resultado)
+    elif escolha == 3:
+        print("Ola")
+    elif escolha == 4:
+        print("Ola")
+    elif escolha == 5:
+        print("Ola")
+    elif escolha == 6:
+        print("Ola")
+    elif escolha == 7:
+        print("Agradecemos por seu tempo, esperamos por sua presença novamente!")
+        break
+    else:
+        print("Caractere inválido")
