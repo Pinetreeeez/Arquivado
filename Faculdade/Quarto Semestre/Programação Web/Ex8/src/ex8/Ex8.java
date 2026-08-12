6e694d56d3ede34029ed9d16edf506bf35c6323f
Pinetreezo <eliosavoine2014@gmail.com>
2026-08-12 11:09:42 -0300

    mini

---
+++ Faculdade/Quarto Semestre/Programação Web/Ex8/src/ex8/Ex8.java
@@ -0,0 +1,12 @@
+package ex8;
+
+import java.util.Scanner;
+
+public class Ex8 {
+
+    public static void main(String[] args) {
+        
+        Scanner ler = new Scanner (System.in);
+    }
+    
+}
